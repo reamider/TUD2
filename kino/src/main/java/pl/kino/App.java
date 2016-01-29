74e1cabecbe5d01ed367952b887116a75df5a745
Sebastian <sebastian.matyjas@gmail.com>
2016-01-29 18:57:51 +0100

    Wygenerowanie projektu za pomocą instrukcji z wykładu
    Wygenerowanie projektu za pomocą instrukcji z wykładu

---
+++ kino/src/main/java/pl/kino/App.java
@@ -0,0 +1,13 @@
+package pl.kino;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
