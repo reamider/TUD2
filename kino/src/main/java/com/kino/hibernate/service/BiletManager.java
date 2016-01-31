bcfc21a5989ed403a95b32bd1d0cc537165d21ef
Sebastian <sebastian.matyjas@gmail.com>
2016-01-31 18:48:42 +0100

    Dodanie managera do operacji w Bilecie.
    Dodanie managera do operacji w Bilecie.

---
+++ kino/src/main/java/com/kino/hibernate/service/BiletManager.java
@@ -0,0 +1,48 @@
+package com.kino.hibernate.service;
+import com.kino.hibernate.domain.Bilet;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class BiletManager implements BiletDAO{
+	@Autowired
+	private SessionFactory session;
+	
+	public SessionFactory getSessionFactory(){
+		return session;
+	}	
+	public void setSessionFactory(SessionFactory session){
+		this.session = session;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Bilet> pobierzBilety(){
+		return session.getCurrentSession().getNamedQuery("Bilet.pobierzBilety").list();
+	}
+	
+	@Override
+	public Bilet pobierzBiletPoID(Bilet bilet){
+		return (Bilet) session.getCurrentSession().get(Bilet.class, bilet.getidBilet());
+	}
+	
+	@Override
+	public void dodajBilet(Bilet bilet){
+		bilet.setidBilet(null);
+		session.getCurrentSession().persist(bilet);
+	}
+	
+	@Override
+	public void edytujBilet(Bilet bilet){
+		session.getCurrentSession().update(bilet);
+	}
+	
+	@Override
+	public void usunBilet(Bilet bilet){
+		session.getCurrentSession().delete(bilet);
+	}
+}
