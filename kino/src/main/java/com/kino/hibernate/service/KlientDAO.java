f7e51af7542485f680f0602e578219f846e81c58
Sebastian <sebastian.matyjas@gmail.com>
2016-01-31 18:38:00 +0100

    Interfejs DAO dla Klienta
    Interfejs DAO dla Klienta

---
+++ kino/src/main/java/com/kino/hibernate/service/KlientDAO.java
@@ -0,0 +1,14 @@
+package com.kino.hibernate.service;
+import com.kino.hibernate.domain.Bilet;
+import com.kino.hibernate.domain.Klient;
+import java.util.List;
+
+public interface KlientDAO{
+	List<Klient> pobierzKlientow();
+	Klient pobierzKlientPoID(Klient klient);
+	List<Klient> pobierzKlientPoImie(String imie);
+	void dodajKlient(Klient klient);
+	void dodajKlientDoBilet(Klient klient, Bilet bilet);
+	void edytujKlient(Klient klient);
+	void usunKlient(Klient klient);
+}
