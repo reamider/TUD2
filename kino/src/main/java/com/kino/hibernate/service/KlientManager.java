e8254534e93b13f4ae46f92d518421c85fcef7c0
Sebastian <sebastian.matyjas@gmail.com>
2016-01-31 19:44:20 +0100

    Plik manager do operacji w kliencie
    Manager operacji do klienta

---
+++ kino/src/main/java/com/kino/hibernate/service/KlientManager.java
@@ -0,0 +1,61 @@
+package com.kino.hibernate.service;
+import com.kino.hibernate.domain.Klient;
+import com.kino.hibernate.domain.Bilet;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class KlientManager implements KlientDAO{
+	@Autowired
+	private SessionFactory session;
+	
+	public SessionFactory getSessionFactory(){
+		return session;
+	}	
+	public void setSessionFactory(SessionFactory session){
+		this.session = session;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Klient> pobierzKlientow() {
+		return session.getCurrentSession().getNamedQuery("Klient.pobierzKlientow").list();
+	}
+
+	@Override
+	public Klient pobierzKlientPoID(Klient klient) {
+		return (Klient) session.getCurrentSession().get(Klient.class, klient.getidKlient());
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Klient> pobierzKlientPoImie(String imie) {
+		return session.getCurrentSession().getNamedQuery("Klient.pobierzKlientPoImie").setString("imie", imie).list();
+	}
+	
+	@Override
+	public void dodajKlient(Klient klient) {
+		klient.setidKlient(null);
+		session.getCurrentSession().persist(klient);	
+	}
+
+	@Override
+	public void dodajKlientDoBilet(Klient klient, Bilet bilet) {
+		Bilet b = (Bilet) session.getCurrentSession().get(Bilet.class, bilet.getidBilet());
+		b.getKlients().add(klient);
+	}
+	
+	@Override
+	public void edytujKlient(Klient klient) {
+		session.getCurrentSession().update(klient);
+	}
+
+	@Override
+	public void usunKlient(Klient klient) {
+		session.getCurrentSession().delete(klient);	
+	}
+}
